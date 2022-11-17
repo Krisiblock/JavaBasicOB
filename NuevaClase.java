28db3ad48cfb9db30f1959cb647bb7c4c8036b8b
Kristiyan <117295078+Krisiblock@users.noreply.github.com>
2022-11-17 20:43:08 +0100

    Ejercicio 1 OB

---
+++ NuevaClase.java
@@ -0,0 +1,16 @@
+package Ejercicio1;
+
+public class NuevaClase {
+
+    public static void main(String[] args) {
+        int numero = 4;
+        double decimal = 3.55;
+        String nombre = "Antonio";
+        boolean verdadero = true;
+
+        System.out.println(numero);
+        System.out.println(decimal);
+        System.out.println(nombre);
+        System.out.println(verdadero);
+    }
+}
